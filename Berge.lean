bf38e75e03e861355b2809c1c4598863f59c3d41
Lessness Randomness <lessnessr@gmail.com>
2023-10-26 16:30:56 +0300

    Formulated symmetric difference of two graphs

--- Berge.lean
+++ Berge.lean
@@ -16,6 +16,35 @@ def AlternatingWalk {V} {G: SimpleGraph V} (M: G.Subgraph) {x y: V} (b: Bool) (W
 def AugmentingPath {V} {G: SimpleGraph V} (M: G.Subgraph) {x y: V} (P: G.Path x y): Prop :=
   x ∉ M.support ∧ y ∉ M.support ∧ AlternatingWalk M false P.val
 
+
+
+def XOR (P1 P2: Prop): Prop := P1 ∧ ¬ P2 ∨ ¬ P1 ∧ P2
+
+theorem aux {V} {G: SimpleGraph V} (M: G.Subgraph): ∀ (x y: V), M.Adj x y → G.Adj x y := by
+  induction M
+  simp
+  assumption
+
+def GraphSymmDiff {V} {G: SimpleGraph V} (M1 M2: G.Subgraph): G.Subgraph :=
+  SimpleGraph.Subgraph.mk
+  (@Set.univ V)
+  (fun x y => XOR (M1.Adj x y) (M2.Adj x y))
+  (by
+    intros x y H
+    simp [XOR] at H
+    let W1 := aux M1 x y
+    let W2 := aux M2 x y
+    tauto)
+  (by
+    intros x y H
+    simp)
+  (by
+    simp [Symmetric, XOR]
+    intros x y H
+    tauto)
+
+
+
 theorem Berge's_lemma {V} [Finite V] {G: SimpleGraph V} {M: SimpleGraph.Subgraph G} (H: M.IsMatching):
   IsMaximum H ↔ (∀ (x y: V) (P: SimpleGraph.Path G x y), ¬ AugmentingPath M P) := by
   sorry
